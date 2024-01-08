ed520d6b31bafdac0541ccf849d647fe3f3f5af7
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-09 01:26:42 +0200

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
-return (NULL);
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
